2c8dcc5c4c5591689b87ba658946aaafdce28c89
Kun@Stream <kun@fluencysecurity.com>
2022-12-12 10:56:54 -0500

    set firehose buffer_size to 5MB

--- modules/region_config/main.tf
+++ modules/region_config/main.tf
@@ -128,6 +128,7 @@ resource "aws_kinesis_firehose_delivery_stream" "extended_s3_stream" {
   extended_s3_configuration {
     role_arn   = aws_iam_role.FluencyMetricStreamS3Role.arn
     bucket_arn = aws_s3_bucket.metricbucket.arn
+    buffer_size = 5
     buffer_interval = 60
   }
 }
